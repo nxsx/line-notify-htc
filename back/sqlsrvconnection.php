3f351c22103120c9f58cfa585c43bb8ee1127484
Thanat Dampawanwong <nxzxover@gmail.com>
2021-10-02 10:01:27 +0700

    chage any config

--- back/sqlsrvconnection.php
+++ back/sqlsrvconnection.php
@@ -1,8 +1,8 @@
 <?php
 
-define("HRPORTAL_CONNECTION", "sqlsrv:server=00000;Database=00000");
-define("HRPORTAL_USERADMIN", "00000");
-define("HRPORTAL_PASSWORDADMIN", "00000");
+define("HRPORTAL_CONNECTION", "sqlsrv:server=111111;Database=111111");
+define("HRPORTAL_USERADMIN", "111111");
+define("HRPORTAL_PASSWORDADMIN", "111111");
 
 class DatabaseConnection 
 {
